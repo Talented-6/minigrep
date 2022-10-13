e6510d8117c7ffca1c1839b374a2ce01364d18c6
= <=>
2022-10-14 00:08:50 +0800

    complete

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use minigrep::Config;
+use std::env;
+use std::process;
+
 fn main() {
-    println!("Hello, world!");
+    // env::args_os() // 处理可能有错误的参数输入
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing argument: {}", err);
+        process::exit(1);
+    });
+
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {}", e);
+        process::exit(1);
+    };
 }
